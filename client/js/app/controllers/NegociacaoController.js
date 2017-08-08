2d42522318d7062af90b094097938f0dc21bdcdf
twsouza <taynan.tws@gmail.com>
2017-08-08 20:49:12 -0300

    helpers no JS

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,47 +4,29 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._cleanForm();
 
-        //let data = new Date(this._inputData.value.replace(/-/g, ','));
-        // ... (3 pontos) é o spread operator, indica q cada posição do array é uma posição do construtor
-        
-        /* let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map(function(item, index){
-                if(index == 1) return --item;
-                return item;
-            })
-        ); */
-        
-        // arrow function
-        let data = new Date(
-            ...
-            this._inputData.value
-            .split('-')
-            .map((item, index) => item - index % 2)
-        );
+        console.log(this._listaNegociacoes.negociacoes);
+    }
 
-        console.log(data);
-        let negociacao = new Negociacao(
-            data,
-            //this._inputData.value,
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
-
-        console.log(negociacao);
-        this.cleanForm();
     }
 
-    cleanForm() {
-        this._inputData.value = '';
-        this._inputQuantidade.value = '';
-        this._inputValor.value = '';
+    _cleanForm() {
+        this._inputData.value       = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value      = 0.0;
 
         this._inputData.focus();
     }
