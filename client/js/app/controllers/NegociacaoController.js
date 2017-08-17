47e88a62863e9dfc6ffb18b1148bdd7cc2b16beb
twsouza <taynan.tws@gmail.com>
2017-08-16 23:09:09 -0300

    Adotando o "Observer" para a view ser atualizada

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,10 +4,16 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
-        this._negociacoesView.update(this._listaNegociacoes);
+        // this._listaNegociacoes = new ListaNegociacoes(this, function(model){
+        //     this._negociacoesView.update(model);
+        // });
+
+        this._listaNegociacoes = new ListaNegociacoes(model => 
+            this._negociacoesView.update(model));
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        //this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
@@ -17,7 +23,6 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociação adicionada com sucesso.';
         this._mensagemView.update(this._mensagem);
@@ -39,5 +44,11 @@ class NegociacaoController {
         this._inputValor.value      = 0.0;
 
         this._inputData.focus();
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso.';
+        this._mensagemView.update(this._mensagem.texto);
     }
 }
