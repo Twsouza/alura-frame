54618447e008febc47df2bf456b05a45623c9ab5
twsouza <taynan.tws@gmail.com>
2017-08-23 22:13:25 -0300

    Proxy no NegociacaoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,13 +5,21 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-
-        /*
-        this._listaNegociacoes = new ListaNegociacoes(model =>
-            this._negociacoesView.update(model));
-        */
-
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+			get: function(target, prop, receiver) {
+				if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+					return function() {
+                        console.log(`interceptado ${prop}`);
+
+						Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+					}
+				}
+				return Reflect.get(target, prop, receiver);
+			}
+        });
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
