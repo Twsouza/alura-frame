785e1ad52fc7305e348d7f5486296684a8e28f39
twsouza <taynan.tws@gmail.com>
2017-08-23 23:27:46 -0300

    padrão Factory no JS

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,25 +5,17 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        let self = this;
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-			get: function(target, prop, receiver) {
-				if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
-					return function() {
-                        console.log(`interceptado ${prop}`);
+        this._listaNegociacoes = new Bind (
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia'
+        );
 
-						Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-					}
-				}
-				return Reflect.get(target, prop, receiver);
-			}
-        });
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($('#mensagemView')),
+            'texto'
+        );
     }
 
     adiciona(event) {
@@ -31,7 +23,6 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
 
         this._mensagem.texto = 'Negociação adicionada com sucesso.';
-        this._mensagemView.update(this._mensagem);
 
         this._cleanForm();
     }
@@ -55,6 +46,5 @@ class NegociacaoController {
     apaga() {
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso.';
-        this._mensagemView.update(this._mensagem.texto);
     }
 }
